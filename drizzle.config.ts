fec4126a82d0eac67baee97777a8bb8cbe8060d7
siraku <slstc6666@yahoo.co.jp>
2024-09-28 21:52:49 +0900

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   schema: "./src/config/schema.ts",
   dialect: 'postgresql',
   dbCredentials: {
-    url: "postgresql://ai-kids-story_owner:pP9WOZCoIn3d@ep-twilight-hall-a1y1uxar.ap-southeast-1.aws.neon.tech/ai-kids-story?sslmode=require",
+    url: "",
   },
   verbose: true,
   strict: true,
